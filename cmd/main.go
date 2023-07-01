728f14d7bd9529315363e9c55a38b0c248da1bc9
Glebegor <glebegor2220@gmail.com>
2023-07-02 01:01:27 +0200

    Created data to write to files, need to refind variations to save data and change data. Need to create in more flexability and sizecoverty.

--- cmd/main.go
+++ cmd/main.go
@@ -9,14 +9,14 @@ import (
 
 func main() {
 	mainDirsArray := []string{"/", "/cmd", "/pkg", "/schema", "/configs", "/interfaces", "/pkg/repository", "/pkg/service", "/pkg/handler"}
-	mainFilesArray := []string{"/.env", "/.gitignore", "/server.go", "/server.go", "/README.md", "/DockerFile", "/MakeFile", "/cmd/main.go", "/schema/000001_init.up.sql", "/schema/000001_init.down.sql", "/configs/config.yml", "/pkg/repository/repository.go", "/pkg/service/service.go", "/pkg/handler/handler.go", "/pkg/handler/cors.go"}
+	mainFilesArray := []string{"/.env", "/.gitignore", "/server.go", "/README.md", "/DockerFile", "/MakeFile", "/cmd/main.go", "/schema/000001_init.up.sql", "/schema/000001_init.down.sql", "/configs/config.yml", "/pkg/repository/repository.go", "/pkg/service/service.go", "/pkg/handler/handler.go", "/pkg/handler/cors.go"}
 	path, err := os.Getwd()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 	myAppName := os.Args
-	workWithElems := creating.NewWorkWithElements(myAppName[1], path, mainDirsArray, mainFilesArray, getDataToWrite())
+	workWithElems := creating.NewWorkWithElements(myAppName[1], path, mainDirsArray, mainFilesArray, getDataToWrite(myAppName[1]))
 	fmt.Printf("Started Creating App - %s - by clear arch\n", myAppName[1])
 
 	workWithElems.CreateArch(*workWithElems, myAppName[1], path)
@@ -24,9 +24,21 @@ func main() {
 	workWithElems.WriteInfoForFiles(*workWithElems, myAppName[1], path)
 }
 
-func getDataToWrite() []string {
+func getDataToWrite(appName string) []string {
 	return []string{
 		"Secret_key='Secret_key'",
 		"venv\n.env\nvenv/\n/venv\n",
+		"",
+		fmt.Sprintf("# Project %s", appName),
+		"",
+		"run:\n	go run cmd/main.go",
+		"package main\n\nfunc main(){\n	return\n}",
+		"",
+		"",
+		"",
+		"package repository",
+		"package service",
+		"package handler",
+		"package handler",
 	}
 }
