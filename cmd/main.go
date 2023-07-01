09ff4deb8c011c88ee1b95fc6f53104bcb5640ce
Glebegor <glebegor2220@gmail.com>
2023-07-01 22:13:48 +0200

    Created lib

---
+++ cmd/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	appName := "APP"
+	fmt.Printf("Started Creating App - %s - by clear arch", appName)
+	return
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Glebegor/ACPOG
+
+go 1.20
